b0db743ad96afcfe4164f1cd5f108d85d8e448bb
diegovasquezdev <wasd2d@gmail.com>
2020-03-13 11:43:17 -0300

    Initial commit

---
+++ assets/config.js
@@ -0,0 +1,50 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "",
+        address: "",
+        social: [
+            
+            
+            {
+                icon: 'twitter',
+                link: "https:\/\/www.twitter.com\/d1egovasquez"
+            },
+            
+            
+            {
+                icon: 'instagram',
+                link: "https:\/\/www.instagram.com\/d1egovasquez"
+            },
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "Hola, soy",
+            title: "Diego Vásquez",
+            subtitle: "UX Developer",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
